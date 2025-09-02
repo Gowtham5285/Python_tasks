b95ab73137315e37e836f11444ff2cc8d47af962
Valluri Gowtham Sai <130955392+Gowtham5285@users.noreply.github.com>
2025-09-02 19:44:36 +0530

    Restore vowel extraction functionality

--- dict_comp.py
+++ dict_comp.py
@@ -1,16 +1,17 @@
-# result = ""
+result = ""
 
-# def allVowels(ch):
-#     global result      
-#     result += ch       
-#     return result      
+def allVowels(ch):
+    global result      
+    result += ch       
+    return result      
 
-# a = {"name1": "vamsi", "name2": "ravi"}
+a = {"name1": "vamsi", "name2": "ravi"}
 
-# res = {allVowels(j) for i in a for j in a[i] if j in "aeiouAEIOU"}
+res = {allVowels(j) for i in a for j in a[i] if j in "aeiouAEIOU"}
 
-# print(result)
-def evenNum(eN):
-    print(eN,"is even number")
-nums=[1,2,3,4,5]
-res=[evenNum(i) for i in nums if i%2==0]
+print(result)
+# def evenNum(eN):
+#     print(eN,"is even number")
+# nums=[1,2,3,4,5]
+
+# res=[evenNum(i) for i in nums if i%2==0]
